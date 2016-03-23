b42c948611fac28c8f54fdf065f603645a3daad8
BtLutz <b@btlutz.com>
2016-03-22 21:40:22 -0400

    Beat level 3 algorithmically, no user input after final algorithm correction. Yippie Kay Yay, motherfucker.

--- sell_side.py
+++ sell_side.py
@@ -4,15 +4,15 @@ from pdb import set_trace
 from numpy import average, std
 import time
 
-class order:
-    __init__(self, venue, stock, bid, ask, direction, quantity, filled):
-        self.venue = venue
-        self.stock = stock
-        self.bid = bid
-        self.ask = ask
-        self.direction = direction
-        self.quantity = quantity
-        self.filled = filled
+# class order:
+#     __init__(self, venue, stock, bid, ask, direction, quantity, filled):
+#         self.venue = venue
+#         self.stock = stock
+#         self.bid = bid
+#         self.ask = ask
+#         self.direction = direction
+#         self.quantity = quantity
+#         self.filled = filled
 
 
 api_key = 'af098a75a49ab1f6f358d7af06c089a97a875ffd'
@@ -24,9 +24,9 @@ level_id = 'sell-side'
 api.gm_start(level_id)
 
 
-my_account = 'AAM312666'
-venue = 'MMTEX'
-target_stock = 'TBI'
+my_account = 'MFB61240943'
+venue = 'MUBEX'
+target_stock = 'LKH'
 
 previous_orderbook = api.stock_orderbook(venue, target_stock)
 
@@ -53,44 +53,50 @@ while True:
             average_bid_qty = int(average(bid_qtys))
             average_ask_qty = int(average(ask_qtys))
 
+            if (average_bid_qty - average_ask_qty > 10):
+                average_ask_qty = average_ask_qty + 25
             bid_to_use = int(average_bid_price + std(bid_prices))
             ask_to_use = int(average_ask_price - std(ask_prices))
 
 
             print("bid: %s | ask: %s" % (bid_to_use, ask_to_use))
 
-            for order in orders:
-                if average_ask >= order.ask and direction == 'buy':
-                    api.stock_order(venue, my_account, target_stock, \
-                    order.ask, order.quantity, 'sell', 'limit')
-                elif average_bid >= order.bid and direction == 'sell':
-                    api.stock_order(venue, my_account, target_stock, \
-                    order.bid, order.quantity, 'buy', 'limit')
+            # for order in orders:
+            #     if average_ask >= order.ask and direction == 'buy':
+            #         api.stock_order(venue, my_account, target_stock, \
+            #         order.ask, order.quantity, 'sell', 'limit')
+            #     elif average_bid >= order.bid and direction == 'sell':
+            #         api.stock_order(venue, my_account, target_stock, \
+            #         order.bid, order.quantity, 'buy', 'limit')
 
-            api.stock_order(venue, my_account, target_stock, bid_to_use, \
-                            int(average_bid_qty / 10), 'buy', 'limit')
-            time.sleep(1)
-            api.stock_order(venue, my_account, target_stock, ask_to_use, \
-                            int(average_ask_qty / 10), 'sell', 'immediate-or-cancel')
-            time.sleep(1)
+            # Instead of bidding at average price, buy it at the last order's price/ask_to_use
+            # then.
+            api.stock_order(venue, my_account, target_stock, bid_prices[0], \
+                            50, 'buy', 'limit')
+            time.sleep(3)
+            api.stock_order(venue, my_account, target_stock, ask_prices[0], \
+                            50, 'sell', 'limit')
+            time.sleep(3)
             my_orders = api.account_stock_orders(venue, my_account, \
                                                  target_stock)
-
-            shorts = [order['qty'] for order in my_orders['orders'] \
-                        if order['direction'] == 'sell']
-            longs  = [order['qty'] for order in my_orders['orders'] \
-                        if order['direction'] == 'buy']
-
-            if abs(sum(longs) - sum(shorts)) > danger_zone:
-                print(sum(longs) - sum(shorts))
-                if sum(longs) - sum(shorts) > zero:
-                    # sell
-                    api.stock_order(venue, my_account, target_stock, \
-                    bid_to_use, danger_zone, 'sell', 'market')
-                    time.sleep(1)
-                elif sum(longs) - sum(shorts) < zero:
-                    # buy
-                    api.stock_order(venue, my_account, target_stock, \
-                    ask_to_use, danger_zone, 'buy', 'market')
-                    time.sleep(1)
+            # try:
+            #     shorts = [order['qty'] for order in my_orders['orders'] \
+            #                 if order['direction'] == 'sell']
+            #     longs  = [order['qty'] for order in my_orders['orders'] \
+            #                 if order['direction'] == 'buy']
+            #
+            #     if abs(sum(longs) - sum(shorts)) > danger_zone:
+            #         print(sum(longs) - sum(shorts))
+            #         if sum(longs) - sum(shorts) > zero:
+            #             # sell
+            #             api.stock_order(venue, my_account, target_stock, \
+            #             bid_to_use, danger_zone, 'buy', 'market')
+            #             time.sleep(1)
+            #         elif sum(longs) - sum(shorts) < zero:
+            #             # buy
+            #             api.stock_order(venue, my_account, target_stock, \
+            #             ask_to_use, danger_zone, 'sell', 'market')
+            #             time.sleep(1)
+            # except:
+            #     pass
             previous_orderbook = orderbook
